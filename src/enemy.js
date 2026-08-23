ebbd8feda0a3c6a4723c21cf5e6dcdce34292a62
MinString <119635772+MinString@users.noreply.github.com>
2026-08-23 12:27:58 +0800

    Add simple square enemy

---
+++ src/enemy.js
@@ -0,0 +1,7 @@
+export function createEnemy(scene, x, y) {
+  const enemy = scene.physics.add.rectangle(x, y, 32, 32, 0xff0000);
+  scene.physics.add.existing(enemy);
+  enemy.body.setVelocityX(80);
+  enemy.body.setCollideWorldBounds(true);
+  return enemy;
+}
