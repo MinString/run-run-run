c91c46111fd01664335b2a1e346967ef7e527eda
MinString <119635772+MinString@users.noreply.github.com>
2026-08-23 12:26:18 +0800

    Expand level and export platforms for collision

--- src/level.js
+++ src/level.js
@@ -1,12 +1,20 @@
 export function createLevel(scene) {
-  const platforms = [
+  const platforms = [];
+
+  const data = [
     [400, 420, 800, 40],
     [250, 330, 180, 20],
-    [600, 260, 160, 20]
+    [600, 260, 160, 20],
+    [950, 350, 260, 20],
+    [1350, 280, 220, 20],
+    [1750, 380, 300, 20]
   ];
 
-  platforms.forEach(([x, y, w, h]) => {
+  data.forEach(([x, y, w, h]) => {
     const p = scene.add.rectangle(x, y, w, h, 0x666666);
     scene.physics.add.existing(p, true);
+    platforms.push(p);
   });
+
+  return platforms;
 }
