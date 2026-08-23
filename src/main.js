588f3f79521ebb33982bd80a6e3b52fb572afc10
MinString <119635772+MinString@users.noreply.github.com>
2026-08-23 12:26:11 +0800

    Add camera scrolling and win flag support

--- src/main.js
+++ src/main.js
@@ -13,22 +13,40 @@ const config = {
 
 let player;
 let cursors;
+let flag;
 
 new Phaser.Game(config);
 
 function create() {
   const scene = this;
-  createLevel(scene);
+
+  scene.physics.world.setBounds(0, 0, 2400, 450);
+  scene.cameras.main.setBounds(0, 0, 2400, 450);
+
+  const platforms = createLevel(scene);
 
   player = scene.physics.add.rectangle(120, 300, 32, 48, 0xffffff);
   scene.physics.add.existing(player);
   player.body.setCollideWorldBounds(true);
 
+  platforms.forEach(platform => {
+    scene.physics.add.collider(player, platform);
+  });
+
+  flag = scene.physics.add.rectangle(2200, 350, 20, 80, 0xff0000);
+  scene.physics.add.existing(flag, true);
+
+  scene.physics.add.overlap(player, flag, () => {
+    console.log('You win!');
+  });
+
+  scene.cameras.main.startFollow(player, true);
   cursors = scene.input.keyboard.createCursorKeys();
 }
 
 function update() {
   if (!player) return;
+
   if (cursors.left.isDown) player.body.setVelocityX(-200);
   else if (cursors.right.isDown) player.body.setVelocityX(200);
   else player.body.setVelocityX(0);
