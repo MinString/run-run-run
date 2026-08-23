eccaefd14175e47a01f69c5b06679f5eb7fa5c2f
MinString <119635772+MinString@users.noreply.github.com>
2026-08-23 13:03:22 +0800

    Fix enemy physics object creation

--- src/enemy.js
+++ src/enemy.js
@@ -1,5 +1,5 @@
 export function createEnemy(scene, x, y) {
-  const enemy = scene.physics.add.rectangle(x, y, 32, 32, 0xff0000);
+  const enemy = scene.add.rectangle(x, y, 32, 32, 0xff0000);
   scene.physics.add.existing(enemy);
   enemy.body.setVelocityX(80);
   enemy.body.setCollideWorldBounds(true);
