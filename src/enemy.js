18c32ea8d9e49c26a28b56f42e3ccb66576fed9d
MinString <119635772+MinString@users.noreply.github.com>
2026-08-25 19:06:46 +0800

    chore: remove legacy Phaser source

--- src/enemy.js
+++
@@ -1,7 +0,0 @@
-export function createEnemy(scene, x, y) {
-  const enemy = scene.add.rectangle(x, y, 32, 32, 0xff0000);
-  scene.physics.add.existing(enemy);
-  enemy.body.setVelocityX(80);
-  enemy.body.setCollideWorldBounds(true);
-  return enemy;
-}
