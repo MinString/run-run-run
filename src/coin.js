a8f65767f7537db44f065f7e242e52fe25f3a581
MinString <119635772+MinString@users.noreply.github.com>
2026-08-25 19:06:41 +0800

    chore: remove legacy Phaser source

--- src/coin.js
+++
@@ -1,6 +0,0 @@
-export function createCoin(scene, x, y) {
-  const coin = scene.add.rectangle(x, y, 18, 18, 0xffff00);
-  scene.physics.add.existing(coin, true);
-  coin.isCoin = true;
-  return coin;
-}
